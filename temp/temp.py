0ca061657b977fbcfc52d81c8a315a49578f8c8f
jinred9 <jinred9@gmail.com>
2023-03-04 17:26:36 +0900

    update

--- temp/temp.py
+++ temp/temp.py
@@ -1,28 +1,24 @@
-matrix = [[i for i in range(j*10, (j+1)*10)] for j in range(10)]
+# def subset_bit(idx, n):
+#     global count
+#     if idx == n:
+#         print(visited)
+#         count += 1
+#     else:
+#         visited[idx] = 1
+#         subset_bit(idx+1, n)
+#         visited[idx] = -1
+#         subset_bit(idx+1, n)
 
-dr = [1,-1,0,0]
-dc = [0,0,-1,1]
+# n = 4
+# visited = [False for _ in range(n)]
+# subset = []
+# count = 0
+# subset_bit(0, n)
+# print(count)
 
-r,c = 5,5
+a = []
+for i in range(1, 101):
+    a.append(i)
+print(sum(a))
+print(sum(a)/len(a))
 
-print(f'현재: {matrix[r][c]}')
-
-for idx in range(4):
-    nr = r + dr[idx]
-    nc = c + dc[idx]
-
-    print(matrix[nr][nc])
-print(matrix)
-
-[[0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 
- [10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 
- [20, 21, 22, 23, 24, 25, 26, 27, 28, 29], 
- [30, 31, 32, 33, 34, 35, 36, 37, 38, 39], 
- [40, 41, 42, 43, 44, 45, 46, 47, 48, 49], 
- [50, 51, 52, 53, 54, 55, 56, 57, 58, 59], 
- [60, 61, 62, 63, 64, 65, 66, 67, 68, 69], 
- [70, 71, 72, 73, 74, 75, 76, 77, 78, 79], 
- [80, 81, 82, 83, 84, 85, 86, 87, 88, 89], 
- [90, 91, 92, 93, 94, 95, 96, 97, 98, 99]]
-
-import numpy as np
