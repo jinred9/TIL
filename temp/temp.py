61bea161750e0f17a1a28f7b54107484ab9cf051
jinred9 <jinred9@gmail.com>
2023-03-21 03:18:34 +0900

    til_20230321

--- temp/temp.py
+++ temp/temp.py
@@ -1,24 +1,4 @@
-# def subset_bit(idx, n):
-#     global count
-#     if idx == n:
-#         print(visited)
-#         count += 1
-#     else:
-#         visited[idx] = 1
-#         subset_bit(idx+1, n)
-#         visited[idx] = -1
-#         subset_bit(idx+1, n)
+import pandas as pd
 
-# n = 4
-# visited = [False for _ in range(n)]
-# subset = []
-# count = 0
-# subset_bit(0, n)
-# print(count)
-
-a = []
-for i in range(1, 101):
-    a.append(i)
-print(sum(a))
-print(sum(a)/len(a))
-
+df = pd.DataFrame([1,2,3])
+print(df)
