b1c85664039ea6bdf53ffafeab3662a9cbe13ec9
jinred9 <jinred9@gmail.com>
2023-03-25 18:41:03 +0900

    til_20230325

--- temp/temp.py
+++ temp/temp.py
@@ -1,4 +1,18 @@
 import pandas as pd
 
 df = pd.DataFrame([1,2,3])
+# print(df)
+
+df1 = df
+# df1 = pd.DataFrame([3,4,5])
+
+df1[0][1] = 4
+
+# print(df1)
+# print(df)
+
+df2 = df.copy()
+df2[0][1] = 5
 print(df)
+print(df1)
+print(df2)
